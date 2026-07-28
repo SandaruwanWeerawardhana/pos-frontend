c3a8681c1eb5556ba800495037328f4ce06ad976
SandaruwanWeerawardhana <weerawardana@gmail.com>
2026-07-28 10:05:56 +0530

    refactor: enhance layout responsiveness and styling in AppGroupLayout and Terminal components

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -8,7 +8,7 @@ export default function AppGroupLayout({
   return (
     <>
       <AppHeader />
-      <div className="flex min-h-0 flex-1 pt-14">{children}</div>
+      <div className="flex min-h-0 w-full flex-1 overflow-hidden pt-14 [&>*]:min-w-0 [&>*]:flex-1">{children}</div>
     </>
   );
 }
